fb818bc79fda1b3df695d35f67b0f3304d50ca05
HB2333 <e@irec.moe>
2023-04-28 18:34:04 +0800

    Site updated: 2023-04-28 18:34:03

---
+++ js/fomal.js
@@ -0,0 +1,16 @@
+// 复制提醒
+document.addEventListener("copy", function () {
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "哎嘿！复制成功🍬",
+            message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "success",
+            duration: 5000
+          });
+        }
+      })
+  })
